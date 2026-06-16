8a998114a3e9ba634e23d9cc970899df5a9237db
ksunleesh <ksunleesh@gmail.com>
2026-06-16 11:26:00 +0700

    created register validator

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,14 @@
+import z from "zod";
+
+const passwordSchema = z.string().regex(/^[0-9z-zA-Z](6,)$/);
+
+export const registerSchema = z
+  .object({
+    email: z.email("Invalid email address"),
+    password: passwordSchema,
+    confirm: passwordSchema,
+  })
+  .refine((value) => value.password === value.confirm, {
+    error: "Passwords do not match",
+    path: ["password", "confirm"],
+  });
