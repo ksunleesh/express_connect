525cd5aab1ea4705662adfe87295d5d568581e57
ksunleesh <ksunleesh@gmail.com>
2026-06-16 10:43:06 +0700

    add morgan middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import {env} from "./config/env.js";
 import {notFoundMiddleware} from "./middlewares/not-found.middleware.js";
 import {errorMiddleware} from "./middlewares/error.middleware.js";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json()); // needed to read
+
+app.use(morgan("dev"));
 
 app.use(notFoundMiddleware);
 
