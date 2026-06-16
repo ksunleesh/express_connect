fc850272872afe27cd3da56e5d61b04bc49e693e
ksunleesh <ksunleesh@gmail.com>
2026-06-16 17:30:30 +0700

    add create todo schema to todo route

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -2,10 +2,12 @@ import {Router} from "express";
 import {todoController} from "../controllers/todo.controller.js";
 import {authenticate} from "../middlewares/authenticate.middleware.js";
 import {checkRole} from "../middlewares/checkRole.middleware.js";
+import {createTodoSchema} from "../schemas/todoschema.js";
+import {validate} from "../middlewares/validate.middleware.js";
 
 export const todoRouter = Router();
 
 todoRouter.use(authenticate);
 todoRouter.use(checkRole("USER"));
 
-todoRouter.post("/", todoController.create);
+todoRouter.post("/", validate({body: createTodoSchema}), todoController.create);
