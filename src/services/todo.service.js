4f2cb0d8714e09a8f4cdd417fddb36ecfbbefa6f
ksunleesh <ksunleesh@gmail.com>
2026-06-16 17:50:19 +0700

    fix duedate bug

--- src/services/todo.service.js
+++ src/services/todo.service.js
@@ -2,11 +2,11 @@ import {prisma} from "../db/prisma.js";
 
 export const todoService = {};
 
-todoService.create = (userId, input) => {
-  return prisma.todo.create({
+todoService.create = (userId, input) =>
+  prisma.todo.create({
     data: {
       userId,
       ...input,
+      dueDate: input.dueDate ? new Date(input.dueDate) : null,
     },
   });
-};
