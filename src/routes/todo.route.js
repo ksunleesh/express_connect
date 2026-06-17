3cc59ca0093e13f13e56baffd4577520a6e7e67d
ksunleesh <ksunleesh@gmail.com>
2026-06-17 09:45:03 +0700

    create common.schema.js

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -4,6 +4,7 @@ import {authenticate} from "../middlewares/authenticate.middleware.js";
 import {checkRole} from "../middlewares/checkRole.middleware.js";
 import {createTodoSchema, updateTodoSchema} from "../schemas/todoschema.js";
 import {validate} from "../middlewares/validate.middleware.js";
+import {intIdSchema} from "../schemas/common.schema.js";
 
 export const todoRouter = Router();
 
@@ -12,4 +13,4 @@ todoRouter.use(checkRole("USER"));
 
 todoRouter.post("/", validate({body: createTodoSchema}), todoController.create);
 
-todoRouter.put("/:id", validate({body: updateTodoSchema}), todoController.update);
+todoRouter.put("/:id", validate({body: updateTodoSchema, params: intIdSchema}), todoController.update);
